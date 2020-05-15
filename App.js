8339b8e9e87fdcf6933c29c4496d11091597f430
darsshanNair <tysonjasmin@gmail.com>
2020-05-15 14:26:34 +0800

    Fix formatting issues

--- App.js
+++ App.js
@@ -7,73 +7,84 @@ import {
   TouchableOpacity,
   NativeModules,
   Dimensions,
-  TextInput
+  TextInput,
 } from 'react-native';
-import { NetworkInfo } from 'react-native-network-info';
+import {NetworkInfo} from 'react-native-network-info';
 
 class App extends React.Component {
   state = {
-    password: ""
+    password: '',
   };
 
   handlePassword = (text) => {
-    this.setState({ password: text })
-  }
+    this.setState({password: text});
+  };
 
   onPressStart = () => {
     var espBridge = NativeModules.ESPBridge;
-    var type = "esptouch";
-    var deviceSSID = "";
-    var deviceBSSID = "";
+    var type = 'esptouch';
+    var deviceSSID = '';
+    var deviceBSSID = '';
     var wifiPassword = this.state.password;
 
-    NetworkInfo.getBSSID().then(bssid => {
-      deviceBSSID = bssid
+    NetworkInfo.getBSSID().then((bssid) => {
+      deviceBSSID = bssid;
     });
 
-    NetworkInfo.getSSID().then(ssid => {
-      deviceSSID = ssid
+    NetworkInfo.getSSID().then((ssid) => {
+      deviceSSID = ssid;
     });
 
-    espBridge.start({
-      type: type,
-      ssid: deviceSSID,
-      bssid: deviceBSSID,
-      password: wifiPassword,
-      timeout: 50000
-    }).then(function (results) {
-      console.log(results);
-    }).catch(function (error) {
-      console.log(error);
-    });
-  }
+    espBridge
+      .start({
+        type: type,
+        ssid: deviceSSID,
+        bssid: deviceBSSID,
+        password: wifiPassword,
+        timeout: 50000,
+      })
+      .then(function (results) {
+        console.log(results);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   onPressStop = () => {
     var espBridge = NativeModules.ESPBridge;
     espBridge.stop();
-  }
+  };
 
   render() {
     const screenHeight = Math.round(Dimensions.get('window').height);
     return (
       <SafeAreaView>
         <View style={styles.container}>
-          <View style={[styles.buttonsContainer, { marginTop: screenHeight / 3 }]}>
-            <TextInput style={styles.input}
+          <View
+            style={[styles.buttonsContainer, {marginTop: screenHeight / 3}]}>
+            <TextInput
+              style={styles.input}
               underlineColorAndroid="transparent"
               placeholder="Wifi Password"
               placeholderTextColor="#9a73ef"
               autoCapitalize="none"
-              onChangeText={this.handlePassword} />
-            <TouchableOpacity onPress={this.onPressStart} style={styles.buttonStyle} disabled = {this.state.password === "" ? true : false}>
-              <Text style={{ color: "#ffffff", fontWeight: "bold" }}>
+              onChangeText={this.handlePassword}
+            />
+            <TouchableOpacity
+              onPress={this.onPressStart}
+              style={styles.buttonStyle}
+              disabled={this.state.password === '' ? true : false}>
+              <Text style={{color: '#ffffff', fontWeight: 'bold'}}>
                 Start ESP Bridge
-            </Text>
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={this.onPress} style={[styles.buttonStyle, { marginTop: 20 }]}>
-              <Text style={{ color: "#ffffff", fontWeight: "bold" }}>
+            <TouchableOpacity
+              onPress={this.onPress}
+              style={[styles.buttonStyle, {marginTop: 20}]}>
+              <Text style={{color: '#ffffff', fontWeight: 'bold'}}>
                 Stop ESP Bridge
-            </Text>
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -87,10 +98,10 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   buttonsContainer: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonStyle: {
-    backgroundColor: "#7892c2",
+    backgroundColor: '#7892c2',
     elevation: 10,
     padding: 20,
     borderRadius: 10,
@@ -100,7 +111,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: '#7a42f4',
     borderWidth: 1,
-    width: "70%"
+    width: '70%',
   },
 });
 
